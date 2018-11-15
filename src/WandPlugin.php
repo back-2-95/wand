beea3d1964ad14edd41f520f8c64168154609d25
Marko Korhonen <marko.korhonen@druid.fi>
2018-11-15 16:45:09 +0200

    question/answer

--- src/WandPlugin.php
+++ src/WandPlugin.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Druidfi\Composer;
 
 use Composer\Composer;
@@ -15,6 +17,21 @@ class WandPlugin implements PluginInterface, EventSubscriberInterface
     public function activate(Composer $composer, IOInterface $io)
     {
         $this->composer = $composer;
+
+        $query = [
+            sprintf(
+                "\n  <question>%s</question>\n",
+                'What type of installation would you like?'
+            ),
+            "  [<comment>1</comment>] Minimal (no default middleware, templates, or assets; configuration only)\n",
+            "  [<comment>2</comment>] Flat (flat source code structure; default selection)\n",
+            "  [<comment>3</comment>] Modular (modular source code structure; recommended)\n",
+            '  Make your selection <comment>(2)</comment>: ',
+        ];
+
+        $answer = $io->ask(implode($query), '2');
+
+        $io->write('<error>You answered '. $answer .'</error>');
     }
 
     /**
