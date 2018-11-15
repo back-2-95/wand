405b91d65e566186d0dba43cb426fbb2be5cc5f7
Marko Korhonen <marko.korhonen@druid.fi>
2018-11-15 15:58:33 +0200

    Skeleton methods

--- src/WandPlugin.php
+++ src/WandPlugin.php
@@ -2,10 +2,26 @@
 
 namespace Druidfi\Composer;
 
+use Composer\Composer;
 use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 class WandPlugin implements PluginInterface, EventSubscriberInterface
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+    }
 
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [];
+    }
 }
